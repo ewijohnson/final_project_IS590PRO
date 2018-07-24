590ac9a03f22f31bc441e8a6af3d5fd2b7b3a5d6
Eric Johnson <ewijohnson1@gmail.com>
2018-07-23 22:49:24 -0500

    scrapes basic webpage for pokemon and encounter rates - still needs updating for more complex routes (i.e., every route but the first)

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-# testing
+from lxml import html
+import requests
+
+url = 'https://bulbapedia.bulbagarden.net/wiki/Kalos_Route_2'
+page = requests.get(url)
+tree = html.fromstring(page.content)
+
+title = tree.xpath("//h1[@id='firstHeading']/text()")
+print(title[0])
+
+
+pokemon = tree.xpath("//tr[@style='text-align:center;']/th/a/span[text()='X']/../../../td/table/tr/td/a/span[text() != 'Grass']/text()")
+
+rates = tree.xpath("//tr[@style='text-align:center;']/th/a/span[text()='X']/../../../td[@colspan='4']/text()")
+
+# print(pokemon)
+
+poke_rate_dict = {}
+poke_counter = 0
+
+for rate in rates:
+    rate = (rate.strip().strip('%'))
+    rate = int(rate)
+    poke_rate_dict[(pokemon[poke_counter])] = rate
+    poke_counter += 1
+
+print(poke_rate_dict)
