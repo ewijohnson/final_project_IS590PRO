1ee6d3878782e09ef7538a7400b79e004b64b0a7
Eric Johnson <ewijohnson1@gmail.com>
2018-07-26 17:36:23 -0500

    combined pokemon with environment type, still many errors to fix also now prints average # of steps per pokemon

--- Main.py
+++ Main.py
@@ -2,6 +2,17 @@ from lxml import html
 import requests
 import random
 
+# Fine for now:
+# Route 2, 3, 6, Santalune Forest
+
+# Places with Issues:
+#   Route 4, 22, Glittering Cave(different rates in red vs purple flowers, three colors of Flabebe,
+#       different locations)
+#   Route 5, 7-21, Connecting Cave, Reflection Cave, Azure Bay, Lost Hotel, Terminus Cave, Frost Cavern,
+#       Pokemon Village, Victory Road same as R 5 (IndexError) - two slightly different reasons though(one with 'rate'
+#       one with join)
+
+
 def dataDownload():
     """This function gets the relevant information on Pokemon encounters and encounter rates from the Bulbapedia
     webpage that lists this information."""
@@ -14,20 +25,48 @@ def dataDownload():
     print(title[0])
 
 
-    pokemon = tree.xpath("//tr[@style='text-align:center;']/th/a/span[text()='X']/../../../td/table/tr/td/a/span[text() != 'Grass']/text()")
+    pokemon = tree.xpath("//tr[@style='text-align:center;']/th/a/span[text()='X']/../../../td/table/tr/td/a/span/text()")
 
     rates = tree.xpath("//tr[@style='text-align:center;']/th/a/span[text()='X']/../../../td[@colspan='4']/text()")
 
-    # print(pokemon)
+    # for poke in pokemon:
+    #     if poke in ['Surfing', 'Fishing']:
+    #         pokemon.remove(poke)
 
+
+
+    print(pokemon)
+
+    combined_pokemon_list = []
+    # Combines Pokemon name with environment type
+    for i in range(0, len(pokemon), 2):
+        combined_pokemon = pokemon[i] + '-' + pokemon[i + 1]
+        combined_pokemon_list.append(combined_pokemon)
+
+    print(combined_pokemon_list)
     poke_rate_dict = {}
     poke_counter = 0
 
     for rate in rates:
         rate = (rate.strip().strip('%'))
-        rate = int(rate)
-        poke_rate_dict[(pokemon[poke_counter])] = rate
+        try:
+            rate = int(rate)
+        except ValueError:
+            pass
+        poke_rate_dict[(combined_pokemon_list[poke_counter])] = rate
         poke_counter += 1
+
+    for pokemon in combined_pokemon_list:
+        if 'Fishing' in pokemon:
+            del poke_rate_dict[pokemon]
+        elif 'Surfing' in pokemon:
+            del poke_rate_dict[pokemon]
+        elif 'Horde Encounter' in pokemon:
+            del poke_rate_dict[pokemon]
+        elif 'Rustling bush' in pokemon:
+            del poke_rate_dict[pokemon]
+
+    print(poke_rate_dict)
 
     return poke_rate_dict
 
@@ -67,14 +106,15 @@ def monteCarloSimulation(pokemon_dictionary):
 
 poke_dict = dataDownload()
 
-# print(poke_dict)
+#print(poke_dict)
 
 all_step_list = []
-for i in range(100000):
+for i in range(10000):
     total_step_counter = monteCarloSimulation(poke_dict)
     all_step_list.append(total_step_counter)
 
 average = sum(all_step_list) / len(all_step_list)
 max = max(all_step_list)
 min = min(all_step_list)
-print(average, max, min)
+print('average:', average, 'max:', max, 'min:', min)
+print('average steps per pokemon:', average/len(poke_dict))
